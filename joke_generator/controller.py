6773b2cbcd525b59a11355ac1739961de342163c
Price_Addison <prica277@hsd.k12.or.us>
2024-03-12 09:03:18 -0700

    attempting to fix controller.py issues

---
+++ joke_generator/controller.py
@@ -0,0 +1,19 @@
+"""
+controller.py
+by Prica277
+Python code to make API connections.
+"""
+
+import requests as re
+
+base_url = "https://official-joke-api.appspot.com"
+endpoint = "/random_joke"
+
+#building the url
+url = base_url + endpoint
+response = re.get(url)
+
+if response.ok:
+    print(response.text)
+else:
+    print(f"There was an error: {response.status_code}")
