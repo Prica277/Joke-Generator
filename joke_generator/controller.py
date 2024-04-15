57a9f2c5b2f8b269462e444b360705c7c8150af7
Price_Addison <prica277@hsd.k12.or.us>
2024-04-15 13:46:27 -0700

    began adding UI

--- joke_generator/controller.py
+++ joke_generator/controller.py
@@ -5,6 +5,10 @@ Python code to make API connections.
 """
 
 import requests as re
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout
+from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtCore import QSize, Qt
 
 base_url = "https://official-joke-api.appspot.com"
 endpoint = "/random_joke"
@@ -17,3 +21,35 @@ if response.ok:
     print(response.text)
 else:
     print(f"There was an error: {response.status_code}")
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Joke Generator")
+
+        self.setFixedSize(QSize(400, 300))
+
+        layout = QGridLayout()
+        layout.addWidget(Color('red'), 0,0)
+        layout.addWidget(Color('green'), 1,0)
+
+        widget = QWidget
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+class Color(QWidget):
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
