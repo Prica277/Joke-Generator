b25a36bbcb2048c74f58a51fcae9c7fae61b1bdf
Price_Addison <prica277@hsd.k12.or.us>
2024-04-23 09:52:27 -0700

    attempting to color widgets

--- joke_generator/interface.py
+++ joke_generator/interface.py
@@ -46,14 +46,22 @@ class MainWindow(QWidget):
         #align the title label
         title_label.setAlignment(Qt.AlignmentFlag.AlignHCenter | 
                                  Qt.AlignmentFlag.AlignTop)
-
+        
+        #coloring title label
+        """
+        BROKEN:
+        title_label.setAutoFillBackground(True)
+        pal = title_label.palette()
+        pal.setColor("195, 171, 208")
+        title_label.setPalette(pal) 
+        
+        #QColor(195, 171, 208)
+        """
         #add title label
-        main_layout.addWidget(title_label, 0, 0)
+        main_layout.addWidget(title_label, 0, 1)
 
         #explaning the use of joke number slider
         explanation_number_label = QLabel("Select amount of jokes (1 or 10)")
-
-        #aligning the explanation label
 
         #adding the explanation label
         main_layout.addWidget(explanation_number_label, 1, 0)
@@ -65,12 +73,10 @@ class MainWindow(QWidget):
         joke_number_slider.setSingleStep(10)
 
         #adding the joke number slider
-        main_layout.addWidget(joke_number_slider, 1, 1)
+        main_layout.addWidget(joke_number_slider, 1, 3)
 
         #explanation_type_label
         explanation_type_label = QLabel("Enter the type of joke:")
-
-        #aligning the explanation label
 
         #adding the explanation label
         main_layout.addWidget(explanation_type_label, 2, 0)
@@ -80,15 +86,45 @@ class MainWindow(QWidget):
         joke_type_input = QLineEdit()
         joke_type_input.setMaxLength(15)
         joke_type_input.setPlaceholderText("Type here!")
-        """
+        
         joke_type_input.returnPressed.connect(self.return_pressed)
         joke_type_input.selectionChanged.connect(self.selection_changed)
         joke_type_input.textChanged.connect(self.text_changed)
         joke_type_input.textEdited.connect(self.text_edited)
-        """
-        #aligning the joke type input box
-
+        
+        #aligning input box
+        
         #adding the joke type input box
+        main_layout.addWidget(joke_type_input, 2, 3)
+
+        #Go button (should switch between layouts)
+        go_button = QPushButton("Go!")
+
+        #aligning go_button
+        
+
+        #adding go_button
+        main_layout.addWidget(go_button, 3, 2)
+
+
+        
+
+    #Works with joke_type_input line edit widget
+    def return_pressed(self):
+        print("Return pressed!")
+        self.joke_type_input().setText("BOOM!")
+
+    def selection_changed(self):
+        print("Selection changed")
+        print(self.joke_type_input().selectedText())
+
+    def text_changed(self, s):
+        print("Text changed...")
+        print(s)
+
+    def text_edited(self, s):
+        print("Text edited...")
+        print(s)
 
 
 app = QApplication(sys.argv)
